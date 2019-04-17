b3416efe65127fc95c7654958c3e801f30e328c5
domekbez <49716804+domekbez@users.noreply.github.com>
2019-04-17 13:44:51 +0200

    Add files via upload

---
+++ Iterator/Graphs.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public interface IGraph
+    {
+        Edge NextEdge();
+        int GetVertices();
+        bool HasNext();
+        void ResetPos();
+
+    }
+    public struct Edge
+    {
+       
+        public int From { get; set; }
+        public int To { get; set; }
+
+        public Edge(int from, int to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+    }
+
+    public class Cycle : IGraph
+    {
+        private int[] vertices;
+        private int pos;
+   
+        public Cycle(int[] vertices)
+        {
+            this.vertices = vertices;
+            pos = 0;
+        }
+        public int GetVertices()
+        {
+            return vertices.Length;
+        }
+
+        public bool HasNext()
+        {
+            if (pos == vertices.Length) return false;
+            return true;
+        }
+
+        public Edge NextEdge()
+        {
+            if(pos<vertices.Length-1)
+            {
+                Edge el = new Edge(pos, pos + 1);
+                pos++;
+                return el;
+            }
+
+            Edge e = new Edge(pos, 0);
+            pos++;
+            return e;
+            
+            
+        }
+
+        public int[] NextVertices(int v)
+        {
+            List<int> l = new List<int>();
+            int i = 0;
+            for (; i < vertices.Length; i++)
+            {
+                if (vertices[i] == v) break;
+            }
+            if (v == vertices.Length) return null;
+            if(v==vertices.Length-1)
+            {
+                l.Add(vertices[0]);
+                return l.ToArray();
+            }
+            l.Add(vertices[i + 1]);
+            return l.ToArray();
+        }
+
+        public void ResetPos()
+        {
+            pos = 0;
+        }
+    }
+
+
+    public class Grid:IGraph
+    {
+        int width;
+        int height;
+        int pos = 0;
+        public Grid(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int GetVertices()
+        {
+            return width * height;
+        }
+
+        public bool HasNext()
+        {
+            if (pos == width * height - 1) return false;
+            return true;
+        }
+
+        public Edge NextEdge()
+        {
+            return new Edge(-1, -1);
+        }
+
+        public void ResetPos()
+        {
+            pos = 0;
+        }
+    }
+
+    public class MatrixGraph:IGraph
+    {
+    
+        int[,] matrix;
+        int pos = 0;
+        
+        public int GetVertices()
+        {
+            return matrix.GetLength(0);
+        }
+        public MatrixGraph(int vertices)
+        {
+            matrix = new int[vertices, vertices];
+        }
+
+        public void AddEdge(int from, int to)
+        {
+            matrix[from, to] = 1;
+        }
+
+        public int[] NextVertices(int v)
+        {
+            List<int> l = new List<int>();
+            for(int i=0;i<matrix.Length;i++)
+            {
+                if (matrix[v, i] == 1) l.Add(i);
+            }
+            return l.ToArray();
+        }
+
+        public Edge NextEdge()
+        {
+
+            if (matrix[(int)(pos/(matrix.GetLength(0))),pos%matrix.GetLength(0)]!=0)
+            {
+                Edge el = new Edge((int)(pos / (matrix.GetLength(0))), pos % matrix.GetLength(0));
+                pos++;
+                return el;
+            }
+            Edge e = new Edge(-1,-1);
+            pos++;
+            return e;
+        }
+
+        public bool HasNext()
+        {
+            if (pos == matrix.GetLength(0) * matrix.GetLength(0)) return false;
+            return true;
+        }
+
+        public void ResetPos()
+        {
+            pos = 0;
+        }
+    }
+
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    class Program
+    {
+        public static void PrintEdges(IGraph g)
+        {
+            while(g.HasNext()==true)
+            {
+                Edge e = g.NextEdge();
+                if (e.From == -1) continue;
+                Console.WriteLine("{0} -> {1}",e.From,e.To);
+
+            }
+        }
+
+        public static bool IsEuler(IGraph g)
+        {
+            g.ResetPos();
+            int[] parzyste = new int[g.GetVertices()];
+            while (g.HasNext() == true)
+            {
+                Edge e = g.NextEdge();
+                if (e.From == -1) continue;
+                int pom = e.From;
+                int pom2 = e.To;
+                if (parzyste[pom] == 0) parzyste[pom] = 1;
+                else if (parzyste[pom] == 1) parzyste[pom] = 0;
+                if (parzyste[pom2] == 0) parzyste[pom2] = 1;
+                else if (parzyste[pom2] == 1) parzyste[pom2] = 0;
+
+
+            }
+            for(int i=0;i<parzyste.Length;i++)
+            {
+                if (parzyste[i] == 1) return false;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Cycle cycle = new Cycle(new int[] { 0, 2, 1, 3 });
+            PrintEdges(cycle);
+            Console.WriteLine("Cycle - is Euler: " + IsEuler(cycle));
+            Console.WriteLine("---");
+            Console.WriteLine("---");
+            //Grid grid = new Grid(4, 3);
+            //PrintEdges(grid);
+            //Console.WriteLine("Grid - is Euler: " + IsEuler(grid));
+            Console.WriteLine("---");
+            MatrixGraph matrixGraph1 = new MatrixGraph(4);
+            matrixGraph1.AddEdge(0, 3);
+            matrixGraph1.AddEdge(1, 2);
+            matrixGraph1.AddEdge(3, 1);
+            PrintEdges(matrixGraph1);
+            Console.WriteLine("MatrixGraph1 - is Euler: " + IsEuler(matrixGraph1));
+            Console.WriteLine("---");
+            MatrixGraph matrixGraph2 = new MatrixGraph(4);
+            matrixGraph2.AddEdge(0, 3);
+            matrixGraph2.AddEdge(3, 2);
+            matrixGraph2.AddEdge(2, 0);
+            matrixGraph2.AddEdge(1, 3);
+            matrixGraph2.AddEdge(3, 1);
+            PrintEdges(matrixGraph2);
+            Console.WriteLine("MatrixGraph2 - is Euler: " + IsEuler(matrixGraph2));
+            Console.WriteLine("---");
+        }
+    }
+}
